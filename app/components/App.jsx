c587204920623962d45fa53102c67af53e8a8caa
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-06 18:18:26 +0100

    Added possibility to add new tasks by clicking button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,20 +3,61 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
+// const notes = [
+//   {
+//     id: uuid.v4(),
+//     task: 'Learn React'
+//   },
+//   {
+//     id: uuid.v4(),
+//     task: 'Do laundry'
+//   }
+// ];
 
-export default () => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
-    <Notes notes={notes} />
-  </div>
-);
+// export default () => (
+//   <div>
+//     <button onClick={() => console.log('add note')}>+</button>
+//     <Notes notes={notes} />
+//   </div>
+// );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+  addNote = () => {
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+}
